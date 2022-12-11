a146648912299702659a25d9d1f42f1c1cd0718c
chandra <chandrapenugonda655@gmail.com>
2022-12-11 14:09:56 +0530

    initial commit

---
+++ app/root.jsx
@@ -0,0 +1,38 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import globalStyles from "~/styles/global.css";
+import acroStyles from "@arco-design/web-react/dist/css/arco.css";
+
+export const meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links = () => [
+  { rel: "stylesheet", href: globalStyles },
+  { rel: "stylesheet", href: acroStyles },
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
